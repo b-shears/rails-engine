f4b3ffa4f5f36879b795ab685a0873a7c6eab5b9
Bryan Shears <brshears@gmail.com>
2022-08-29 22:13:54 -0600

    add simplecov filter to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
 SimpleCov.start
+SimpleCov.add_filter ["app/channels", "app/helpers", "app/mailers", "app/jobs"]
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest

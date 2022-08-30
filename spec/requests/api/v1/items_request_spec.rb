389057a6b9011cf65d4360c8beb5c79f6900ea28
Bryan Shears <brshears@gmail.com>
2022-08-30 17:23:31 -0600

    add items_requests_spec and tests attributes

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    merchant = create(:merchant)
+    create_list(:item, 3, merchant_id: merchant.id)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+    
+   
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(200)
+        
+    expect(items[:data].count).to eq(3)
+
+     items[:data].each do |item|
+            expect(item).to have_key(:id)
+            expect(item[:id]).to be_a(String)
+
+            expect(item[:attributes]).to have_key(:name)
+            expect(item[:attributes][:name]).to be_a(String)
+    end 
+  end
+end

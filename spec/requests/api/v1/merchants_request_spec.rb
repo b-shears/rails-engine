8ca5cc156cd08655423819a9ab706b9d1827a198
Bryan Shears <brshears@gmail.com>
2022-08-30 11:59:03 -0600

    Update get all merchants tests to pass

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -11,20 +11,20 @@ describe "Merchants API" do
 
         merchants = JSON.parse(response.body, symbolize_names: true)
       
-        expect(merchants.count).to eq(3)
+        expect(merchants[:data].count).to eq(3)
 
-        merchants.each do |merchant|
+        merchants[:data].each do |merchant|
             expect(merchant).to have_key(:id)
-            expect(merchant[:id]).to be_an(Integer)
+            expect(merchant[:id]).to be_an(String)
 
-            expect(merchant).to have_key(:name)
-            expect(merchant[:name]).to be_a(String)
+            expect(merchant[:attributes]).to have_key(:name)
+            expect(merchant[:attributes][:name]).to be_a(String)
 
-            expect(merchant).to have_key(:created_at)
-            expect(merchant[:created_at]).to be_a(String) 
+            expect(merchant[:attributes]).to_not have_key(:created_at)
+           
 
-            expect(merchant).to have_key(:updated_at)
-            expect(merchant[:updated_at]).to be_a(String)
+            expect(merchant[:attributes]).to_not have_key(:updated_at)
+            
         end 
     end 
 

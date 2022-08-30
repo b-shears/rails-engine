24cdc2f8e46f318d4c4b57d172c6586024db3132
Bryan Shears <brshears@gmail.com>
2022-08-29 22:09:31 -0600

    add show test to merchant_request_spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,7 +8,7 @@ describe "Merchants API" do
         get '/api/v1/merchants'
 
         expect(response).to be_successful
-        
+
         merchants = JSON.parse(response.body, symbolize_names: true)
       
         expect(merchants.count).to eq(3)
@@ -26,5 +26,38 @@ describe "Merchants API" do
             expect(merchant).to have_key(:updated_at)
             expect(merchant[:updated_at]).to be_a(String)
         end 
+    end 
+
+    it 'can get one merchant by its id' do 
+        id = create(:merchant).id 
+
+        get "/api/v1/merchants/#{id}"
+
+        merchant = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to eq(id)
+
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a(String)
+
+        expect(merchant).to have_key(:created_at)
+        expect(merchant[:created_at]).to be_a(String)
+
+        expect(merchant).to have_key(:updated_at)
+        expect(merchant[:updated_at]).to be_an(String)
+    end 
+
+    it 'can create a new merchant' do 
+        merchant_params = ({
+                        name: 'Bob Ross'
+                        created_at: 'date'
+                        updated_at: 'date_2'
+        })
+    
+     headers = {"CONTENT_TYPE" => "application/json"}
+
     end 
 end 

ff170e6c13dd0915794c2c55d3d299bdf66192a1
Bryan Shears <brshears@gmail.com>
2022-08-29 20:24:15 -0600

    initial commit

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+require 'faker'
+
+describe "Merchants API" do 
+    it 'sends a list of merchants' do 
+        create_list(:merchant, 3)
+        
+        get '/api/v1/merchants'
+
+        expect(response).to be_successful
+    end 
+
+end 

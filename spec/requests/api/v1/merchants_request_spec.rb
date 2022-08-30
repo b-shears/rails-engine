7119741fea68777ed2a41b489345d85ae92180a7
Bryan Shears <brshears@gmail.com>
2022-08-29 21:23:28 -0600

    Add requests spec and merchant model spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,6 +8,23 @@ describe "Merchants API" do
         get '/api/v1/merchants'
 
         expect(response).to be_successful
-    end 
+        
+        merchants = JSON.parse(response.body, symbolize_names: true)
+      
+        expect(merchants.count).to eq(3)
 
+        merchants.each do |merchant|
+            expect(merchant).to have_key(:id)
+            expect(merchant[:id]).to be_an(Integer)
+
+            expect(merchant).to have_key(:name)
+            expect(merchant[:name]).to be_a(String)
+
+            expect(merchant).to have_key(:created_at)
+            expect(merchant[:created_at]).to be_a(String) 
+
+            expect(merchant).to have_key(:updated_at)
+            expect(merchant[:updated_at]).to be_a(String)
+        end 
+    end 
 end 

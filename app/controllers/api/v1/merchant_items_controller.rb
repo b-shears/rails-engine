b4a3277ccf65f27236005d06879feb408bada78f
Bryan Shears <brshears@gmail.com>
2022-08-30 16:22:18 -0600

    add index method to merchantitemscontroller for get merchant items

---
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::MerchantItemsController < ApplicationController
+    def index 
+            merchant = Merchant.find(params[:merchant_id])
+            merchant_items = Item.where("merchant_id = ?", merchant.id)
+            render json: ItemSerializer.new(merchant_items)
+    end 
+end
